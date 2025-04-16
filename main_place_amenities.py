59b0acf9585c91009523cbf6f83b840eeff6963f
Abel Sekibaala <54054280+asekibaala@users.noreply.github.com>
2025-04-16 05:44:08 +0300

    fix bug State

--- main_place_amenities.py
+++ main_place_amenities.py
@@ -1,27 +1,31 @@
 #!/usr/bin/python3
-""" Test link Many-To-Many Place <> Amenity
-"""
-from models import *
+"""Test script for Place and Amenity relationship"""
+from models import storage
+from models.state import State
+from models.city import City
+from models.user import User
+from models.place import Place
+from models.amenity import Amenity
 
-# creation of a State
+# Create a State
 state = State(name="California")
 state.save()
 
-# creation of a City
+# Create a City
 city = City(state_id=state.id, name="San Francisco")
 city.save()
 
-# creation of a User
-user = User(email="john@snow.com", password="johnpwd")
+# Create a User
+user = User(email="bob@hbtn.io", password="bobpwd", first_name="Bob", last_name="Dylan")
 user.save()
 
-# creation of 2 Places
-place_1 = Place(user_id=user.id, city_id=city.id, name="House 1")
+# Create Places
+place_1 = Place(user_id=user.id, city_id=city.id, name="Lovely Place")
 place_1.save()
-place_2 = Place(user_id=user.id, city_id=city.id, name="House 2")
+place_2 = Place(user_id=user.id, city_id=city.id, name="Awesome Spot")
 place_2.save()
 
-# creation of 3 various Amenity
+# Create Amenities
 amenity_1 = Amenity(name="Wifi")
 amenity_1.save()
 amenity_2 = Amenity(name="Cable")
@@ -29,11 +33,11 @@ amenity_2.save()
 amenity_3 = Amenity(name="Oven")
 amenity_3.save()
 
-# link place_1 with 2 amenities
+# Link place_1 with 2 amenities
 place_1.amenities.append(amenity_1)
 place_1.amenities.append(amenity_2)
 
-# link place_2 with 3 amenities
+# Link place_2 with 3 amenities
 place_2.amenities.append(amenity_1)
 place_2.amenities.append(amenity_2)
 place_2.amenities.append(amenity_3)
